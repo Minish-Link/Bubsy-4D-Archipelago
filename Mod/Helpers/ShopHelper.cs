b530077e31230c1f05f155d8b8dc0cc688282f51
Erik Nolan <41801108+Minish-Link@users.noreply.github.com>
2026-09-21 19:53:05 -0400

    Better Shop Menu

--- Mod/Helpers/ShopHelper.cs
+++ Mod/Helpers/ShopHelper.cs
@@ -1,6 +1,8 @@
 ﻿using BubsyArchipelagoMod.Server;
+using Harmony;
 using Il2CppFabraz;
 using Il2CppFabraz.SaveData;
+using Il2CppFabraz.TextSystem;
 using Il2CppFabraz.UI.Atari;
 using MelonLoader;
 
@@ -10,12 +12,14 @@ public static class ShopHelper
     private static GearShopMenu currentMenu;
     private static bool menuIsOpen = false;
     private static GearShopEntry currentEntry;
+    private static bool isInitialized = false;
 
     private static CollectableType yarnCurrency;
     private static CollectableType blueprintCurrency;
     private static CollectableType voidCurrency;
 
     public static List<ItemData> currentItemDatas = new List<ItemData>();
+    public static bool overrideEntries = true;
 
     public static void OpenMenu(ref GearShopMenu newMenu)
     {
@@ -29,22 +33,50 @@ public static class ShopHelper
         menuIsOpen = false;
     }
 
-    public static void InitializeAPItemData(GearShopMenu templates)
+    public static void InitializeAPItemData(ref GearShopMenu templates)
     {
+        if (isInitialized)
+            return;
+        
         yarnCurrency = templates.currencyTypeYarnball;
         blueprintCurrency = templates.currencyTypeBlueprint;
         voidCurrency = templates.outfitsData[10].currencyType;
-        // TODO
+
         currentItemDatas.Clear();
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 8; i++)
         {
             ItemData newData = new ItemData();
-            newData.price = i * 30;
+            newData.price = i * 50;
             newData.currencyType = yarnCurrency;
             newData.descriptionContent = $"Test Description {i}";
             newData.nameContent = $"Test Name {i}";
+            newData.nameContentLocalisation = new TranslationLine();
+            newData.descriptionContentLocalisation = new TranslationLine();
             currentItemDatas.Add(newData);
         }
+        for (int i = 0; i < 12; i++)
+        {
+            ItemData newData = new ItemData();
+            newData.price = i + 1;
+            newData.currencyType = blueprintCurrency;
+            newData.descriptionContent = $"Blueprint Description {i}";
+            newData.nameContent = $"Blueprint Name {i}";
+            newData.nameContentLocalisation = new TranslationLine();
+            newData.descriptionContentLocalisation = new TranslationLine();
+            currentItemDatas.Add(newData);
+        }
+        for (int i = 0; i < 5 ; i++)
+        {
+            ItemData newData = new ItemData();
+            newData.price = (i+1)*3;
+            newData.currencyType = voidCurrency;
+            newData.descriptionContent = $"Void Description {i}";
+            newData.nameContent = $"Void Name {i}";
+            newData.nameContentLocalisation = new TranslationLine();
+            newData.descriptionContentLocalisation = new TranslationLine();
+            currentItemDatas.Add(newData);
+        }
+        isInitialized = true;
     }
 
     public static void OnAPEntryClicked()
@@ -53,17 +85,24 @@ public static class ShopHelper
         {
             return;
         }
-        // TODO
-        MelonLogger.Msg(currentEntry.getItemData.price);
-        if (currentEntry.getItemData.price <= SaveDataManager.Instance.CurrentSaveData.TotalYarnballCount)
+        bool purchased = false;
+
+        if (currentEntry.getItemData.currencyType == yarnCurrency)
+            purchased = currentEntry.getItemData.price <= SaveDataManager.Instance.CurrentSaveData.TotalYarnballCount;
+        else if (currentEntry.getItemData.currencyType == blueprintCurrency)
+            purchased = currentEntry.getItemData.price <= SaveDataManager.Instance.CurrentSaveData.TotalBlueprintsCount;
+        else if (currentEntry.getItemData.currencyType == voidCurrency)
+            purchased = currentEntry.getItemData.price <= SaveDataManager.Instance.CurrentSaveData.TotalVoidFleeceCount;
+
+        if (purchased)
         {
+            // TODO Connect to function that sends checks to the server.
             int index = currentMenu.currentEntries.FindIndex((Il2CppSystem.Predicate<GearShopEntry>)currentEntry.Equals);
             if (index < 0)
                 return;
             MelonLogger.Msg($"Purchased Item {currentEntry.label.text}");
             UnityEngine.Object.Destroy(currentMenu.currentEntries[index].gameObject);
             currentMenu.currentEntries.RemoveAt(index);
-            
         }
     }
 
--- Mod/Patches/Shop/GearShopMenuPatch.cs
+++ Mod/Patches/Shop/GearShopMenuPatch.cs
@@ -7,6 +7,7 @@ using Il2CppFabraz.UI.Atari;
 using MelonLoader;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BubsyArchipelagoMod.Patches.Shop;
 
@@ -16,10 +17,23 @@ public static class GearShopPopulatePatch
 {
     public static bool Prefix(int category, GearShopMenu __instance)
     {
+        if (!ShopHelper.overrideEntries)
+        {
+            return true;
+        }
         MelonLogger.Msg(ConsoleColor.Blue, category);
-        __instance.ClearEntries();
         if (category == 0)
         {
+            if (ShopHelper.overrideEntries)
+            {
+                __instance.ClearEntries();
+                ShopHelper.overrideEntries = false;
+                __instance.PopulateEntries(0, true);
+                __instance.PopulateEntries(1, true);
+                ShopHelper.overrideEntries = true;
+                return false;
+            }
+            /*
             foreach (var upgrade in __instance.upgradesData)
             {
                 GearShopEntry newEntry = UnityEngine.Object.Instantiate(__instance.entryEquipPrefab);
@@ -38,22 +52,24 @@ public static class GearShopPopulatePatch
                 //__instance.bubsyCharacterController
                 __instance.currentEntries.Add(newEntry);
             }
+            */
         }
         else if (category == 1)
         {
             __instance.ClearEntries();
-            ShopHelper.InitializeAPItemData(__instance);
             foreach (ItemData apData in ShopHelper.currentItemDatas)
             {
-                MelonLogger.Msg(apData);
                 GearShopEntry newEntry = UnityEngine.Object.Instantiate(__instance.entryPurchasePrefab);
                 newEntry.transform.SetParent(__instance.contentRoot);
                 newEntry.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-                newEntry.currencyIcon = (UnityEngine.UI.Image)__instance.currencySprites[apData.currencyType].Asset;
-                newEntry.cost.text = apData.price.ToString();
+                //newEntry.cost.text = apData.price.ToString();
+                //newEntry.label.text = apData.nameContent;
+                //newEntry.description.text = apData.descriptionContent;
+                //newEntry.itemData = apData;
+                //newEntry.currencyIcon = __instance.currencySprites[apData.currencyType].
+                newEntry.SetItemData(apData, __instance.currencySprites[apData.currencyType], true);
                 newEntry.label.text = apData.nameContent;
                 newEntry.description.text = apData.descriptionContent;
-                newEntry.itemData = apData;
 
                 FzButton entryButton = newEntry.GetComponent<FzButton>();
                 if (entryButton)
@@ -69,17 +85,28 @@ public static class GearShopPopulatePatch
         return false;
     }
 
-    // IMPORTANT NOTES FOR FUTURE ERIK
-    // Entry game objects and list of currentEntries are separate entities
-    // Instead of trying to make a third category, combine skins and upgrades into one category.
-    // And the AP items in the other category.
-    // Look into adding only obtained outfits and upgrades to the category by manually populating the entries
-    // Use the class's prefabs, and figure out how to add them to the physical menu and not just the list.
-    // Might need to investigate more in Unity Explorer for this.
-    // GearShopMenu.contentRoot
-    public static void Postfix(ref GearShopMenu __instance)
+    public static void Postfix(int category, GearShopMenu __instance)
     {
-        
+        if (category == 1)
+            return;
+        for (int i = __instance.currentEntries.Count - 1; i >= 0; i--)
+        {
+            FzToggle throwaway;
+            if (!__instance.currentEntries[i].IsToggle(out throwaway))
+            {
+                UnityEngine.Object.Destroy(__instance.currentEntries[i].gameObject);
+                __instance.currentEntries.RemoveAt(i);
+            }
+        }
+    }
+}
+
+[HarmonyPatch(typeof(GearShopMenu), nameof(GearShopMenu.ClearEntries))]
+public static class GearShopClearPatch
+{
+    public static bool Prefix()
+    {
+        return ShopHelper.overrideEntries;
     }
 }
 
@@ -90,6 +117,7 @@ public static class GearShopEnablePatch
     {
         ShopHelper.OpenMenu(ref __instance);
         MelonLogger.Msg("Gear Shop Enable");
+        ShopHelper.InitializeAPItemData(ref __instance);
     }
 }
 
